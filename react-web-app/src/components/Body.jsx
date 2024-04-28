218459d616dde10c5e8068ead51468604d9cc7f0
Shouvik Dutta <shodutta@amazon.com>
2024-04-28 13:19:18 +0530

    Carousel fixed

--- react-web-app/src/components/Body.jsx
+++ react-web-app/src/components/Body.jsx
@@ -1,35 +1,30 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
 
 function Body() {
     return (
-        <div id="carouselExampleDark" className="carousel carousel-dark slide">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active" data-bs-interval="2000">
-                    <img src="https://img-global.cpcdn.com/recipes/ce884d070a8abf57/1200x630cq70/photo.jpg" className="d-block w-100" alt="Non Veg" />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Non-Veg deliciuos</h5>
-                    </div>
-                </div>
-                <div className="carousel-item" data-bs-interval="2000">
-                    <img src="https://images.jdmagicbox.com/quickquotes/listicle/listicle_1683697362867_zui6n_1040x500.jpg" className="d-block w-100" alt="Non Veg" />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Non-Veg deliciuos</h5>
-                    </div>
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
+            <Carousel>
+                <Carousel.Item interval={1500}>
+                    <img
+                        className="d-block w-100"
+                        src="https://img-global.cpcdn.com/recipes/ce884d070a8abf57/1040x500cq70/photo.jpg"
+                        alt="Image One"
+                    />
+                    <Carousel.Caption>
+                        <h3>Veggies</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                        className="d-block w-100"
+                        src="https://images.jdmagicbox.com/quickquotes/listicle/listicle_1683697362867_zui6n_1040x500.jpg"
+                        alt="Image Two"
+                    />
+                    <Carousel.Caption>
+                        <h3>Non-Veg delicious</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
     )
 }
 
