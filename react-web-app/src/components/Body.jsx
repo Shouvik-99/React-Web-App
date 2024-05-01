6683c419a10e1ef47e9ffd8a2e09eb970cfb6361
Shouvik Dutta <shodutta@amazon.com>
2024-05-01 18:17:35 +0530

    [CHORE] Fixing Non-Veg image beauty

--- react-web-app/src/components/Body.jsx
+++ react-web-app/src/components/Body.jsx
@@ -8,8 +8,8 @@ function Body() {
                 <Carousel.Item interval={1500}>
                     <img
                         className="d-block w-100"
-                        src="https://img-global.cpcdn.com/recipes/ce884d070a8abf57/1040x500cq70/photo.jpg"
-                        alt="Image One"
+                        src="https://www.livemint.com/lm-img/img/2023/08/07/1600x900/2-0-284323707--ABH0040-c-0_1680507972555_1691433625706.JPG"
+                        alt="Image One" style={{height: "800px", width: "1040px"}}
                     />
                     <Carousel.Caption>
                         <Link to = "/veg-items" style={{color: '#ffffff', textDecoration: 'none', fontSize: '151px'}}>Veggies</Link>
@@ -20,6 +20,7 @@ function Body() {
                         className="d-block w-100"
                         src="https://images.jdmagicbox.com/quickquotes/listicle/listicle_1683697362867_zui6n_1040x500.jpg"
                         alt="Image Two"
+                        style={{height: "800px", width: "1040px"}}
                     />
                     <Carousel.Caption>
                     <Link to = "/non-veg-items" style={{color: '#ffffff', textDecoration: 'none', fontSize: '100px'}}>Non-Veg delicious</Link>
