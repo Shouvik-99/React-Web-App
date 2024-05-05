afbce9e6564c9825b5eff3118cef8139b5864782
Shouvik Dutta <shodutta@amazon.com>
2024-05-05 12:53:58 +0530

    [CHORE] Adding Image static file support

---
+++ react-web-app/public/ImageSource.js
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+
+export const IMAGE_FILE_PATH = '../../public/images/';
+
+export const IMAGE_NAME_TO_IMAGE_SRC_MAP = new Map([
+    ["Panner Kopta", "https://femina.wwmindia.com/content/2023/jun/twalnutandpaneerkofta16876274181687627434.jpg"],
+    ["Chicken Pakora", "https://i.ytimg.com/vi/xlszcRPKDqk/maxresdefault.jpg"],
+    ["French Fries", "https://www.budgetbytes.com/wp-content/uploads/2023/12/air-fryer-french-fries-horizontal-hero-web-ready-1.jpg"],
+    ["mutton korma", "https://kitchenofdebjani.com/wp-content/uploads/2022/11/mutton-korma-recipe-debjanir-rannaghar.jpg"],
+    ["Chilly Chicken", "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=1080/assets/search/usecase/boneless_chilli_chicken_zdish.png"],
+    ["Chicken fly by wings", "https://www.shutterstock.com/image-photo/perfect-chicken-wings-flying-around-600w-1647129430.jpg"]
+]);
