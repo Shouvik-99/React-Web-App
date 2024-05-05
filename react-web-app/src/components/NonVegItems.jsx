b25d27d41a40290452d18fcb768076d65b03c422
@PramilaGhosh <pramila.ghosh@thoughtworks.com>
2024-05-05 13:14:45 +0530

    [FIX] displaying dynamic image from backend

--- react-web-app/src/components/NonVegItems.jsx
+++ react-web-app/src/components/NonVegItems.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import { fetchFoodItems} from '../store/FoodItemsSlice'
 import "./ItemDisplay.css"
+import { IMAGE_FILE_PATH, IMAGE_NAME_TO_IMAGE_SRC_MAP } from '../../public/ImageSource'
+
 
 function NonVegItems() {
   const { foodItems } = useSelector((store) => (store.items))
@@ -14,7 +16,8 @@ function NonVegItems() {
     <div className='cards'>
     {foodItems.filter(item => item.foodType === 'NON-VEG').map((item) =>
         <div key={item.id} className="card" style={{width: '18rem'}}>
-          <img className="img-as-bg" src="https://c8.alamy.com/comp/2PGD175/masala-chicken-curry-top-view-non-veg-food-2PGD175.jpg" />
+          {/* <img className="img-as-bg" src="https://c8.alamy.com/comp/2PGD175/masala-chicken-curry-top-view-non-veg-food-2PGD175.jpg" /> */}
+          <img className="img-as-bg" src={(item.foodImage === null) ? IMAGE_NAME_TO_IMAGE_SRC_MAP.get(item.foodName) : IMAGE_FILE_PATH.concat(item.foodImage)} />
           <div className="card-body">
             <p className="card-text">{item.foodName}</p>
           </div>
--- react-web-app/src/components/VegItems.jsx
+++ react-web-app/src/components/VegItems.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import { fetchFoodItems} from '../store/FoodItemsSlice'
 import "./ItemDisplay.css"
+import { IMAGE_FILE_PATH, IMAGE_NAME_TO_IMAGE_SRC_MAP } from '../../public/ImageSource'
 
 function VegItems() {
   const { foodItems } = useSelector((store) => (store.items))
@@ -15,7 +16,7 @@ function VegItems() {
     <div className='cards'>
     {foodItems.filter(item => item.foodType === 'VEG').map((item) =>
         <div key={item.id} className="card" style={{width: '18rem'}}>
-          <img className="img-as-bg" src="https://img.freepik.com/free-photo/vertical-shot-traditional-indian-paneer-butter-masala-cheese-cottage-curry-black-surface_181624-32001.jpg" />
+          <img className="img-as-bg" src={(item.foodImage === null) ? IMAGE_NAME_TO_IMAGE_SRC_MAP.get(item.foodName) : IMAGE_FILE_PATH.concat(item.foodImage)} />
           <div className="card-body">
             <p className="card-text">{item.foodName}</p>
           </div>
